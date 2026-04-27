54b21ac073005ea0e9db762eb1f41a68eae25784
MangoJuice10 <swordmaster10A@gmail.com>
2026-04-27 08:48:20 +0300

    Make sidebar's logo navigate the user to the home page

--- frontend/src/widgets/sidebar/ui/Sidebar.vue
+++ frontend/src/widgets/sidebar/ui/Sidebar.vue
@@ -2,7 +2,7 @@
 import {useNavigation} from "@/shared/lib/useNavigation.ts";
 import SidebarToggle from "@/widgets/sidebar/ui/SidebarToggle.vue";
 import Logo from "@/shared/ui/logo/Logo.vue";
-import Navlink from "@/widgets/navbar/ui/NavLink.vue";
+import NavLink from "@/widgets/navbar/ui/NavLink.vue";
 import IconLabel from "@/shared/ui/IconLabel.vue";
 
 defineProps<{
@@ -17,13 +17,15 @@ const navigation = useNavigation().getSidebarNavigation();
     <aside v-show="isVisible" class="flex flex-col w-sidebar h-sidebar border-r border-default bg-primary">
       <div class="flex items-center gap-1 w-full h-navbar px-sidebar border-b border-default">
         <SidebarToggle/>
-        <Logo has-logotype logotype-classes="max-lg:hidden" class="shrink-0 py-2"/>
+        <NavLink to="/" class="block h-full min-w-0 max-w-full max-h-full">
+          <Logo has-logotype logotype-classes="max-lg:hidden" class="shrink-0 py-2"/>
+        </NavLink>
       </div>
       <div class="h-full overflow-auto px-sidebar">
         <section v-for="section in navigation" :key="section.id">
           <h3 v-text="section.heading" class="max-w-full my-heading truncate text-muted"/>
           <nav class="flex flex-col items-start gap-5 h-full mb-section">
-            <Navlink v-for="navLink in section.navLinks" :key="navLink.id" :to="navLink.href"
+            <NavLink v-for="navLink in section.navLinks" :key="navLink.id" :to="navLink.href"
                      class="grow-0 w-full font-semibold px-2 py-1.5
                                 border rounded-xl border-transparent hover:border-default hover:bg-hover">
               <IconLabel class="max-w-full">
@@ -34,7 +36,7 @@ const navigation = useNavigation().getSidebarNavigation();
                   <span v-text="navLink.label.value" class="truncate"/>
                 </template>
               </IconLabel>
-            </Navlink>
+            </NavLink>
           </nav>
         </section>
       </div>
